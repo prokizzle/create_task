433210ce00f25e2dde5aa41566208c7744db1dce
Nick Prokesch <nick@prokes.ch>
2015-09-11 16:50:51 -0500

    Format args for omnifocus

--- lib/create_task/omnifocus.rb
+++ lib/create_task/omnifocus.rb
@@ -4,12 +4,18 @@ module CreateTask
   class OmniFocus
     require 'appscript'#;include Appscript
     def self.create(args)
-      args[:due_date] = parse_date(args[:due_date]) if args[:due_date]
-      args[:defer_date] = parse_date(args[:defer_date]) if args[:defer_date]
-
       of = app('OmniFocus')
       dd= of.default_document
-      dd.make(new: :inbox_task, with_properties: args.to_hash)
+      dd.make(new: :inbox_task, with_properties: processed_args(args))
+    end
+
+    def processed_args(args)
+      if args[:start_date]
+        args[:defer_date] = args[:start_date]
+        args.delete(:start_date)
+      end
+      args[:due_date] = parse_date(args[:due_date]) if args[:due_date]
+      args[:defer_date] = parse_date(args[:defer_date]) if args[:defer_date]
+      args
     end
   end
-end
